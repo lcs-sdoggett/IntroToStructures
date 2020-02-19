8d94a4d43e736506e9b368bc3a39bb5c269c34d7
Scott Doggett <sdoggett@lcs.on.ca>
2020-02-19 11:58:10 -0500

    sorry for not commiting often. It's all done. I will commit more next time.

--- IntroToStructures.playground/Pages/5 - Authoring Structures.xcplaygroundpage/Contents.swift
+++ IntroToStructures.playground/Pages/5 - Authoring Structures.xcplaygroundpage/Contents.swift
@@ -33,8 +33,108 @@ import Foundation
  */
 // Begin your work here...
 
+struct Sphere {
+    
+    // MARK: Properties
+    
+    var radius: Double
+    
+    // Return surface area
+    var surfaceArea: Double{
+        return 4 * radius * Double.pi
+    }
+    
+    // Return volume
+    var volume: Double{
+        return 4 / 3 * Double.pi * pow(radius, 3)
+    }
+    
+    
+}
+
+// Create a sphere
+var aSphere = Sphere(radius: 2)
+
+// Find area and volume
+aSphere.surfaceArea
+aSphere.volume
 
 
+struct Rectangle {
+    
+    // MARK: Properties
+    
+    var length: Double
+    var height: Double
+    var width: Double
+    
+    // Return surface area
+    var surfaceArea: Double{
+        return Double(2) * (length*height + width*height + length*width)
+    }
+    
+    // Return volume
+    var volume: Double{
+        return width * length * height
+    }
+}
+
+// Create  a rectangle
+var aRectangle = Rectangle(length: 10, height: 10, width: 10)
+
+// Find surface area and volume
+aRectangle.surfaceArea
+aRectangle.volume
+
+
+struct Circle {
+    
+    // MARK: Properties
+    
+    var radius: Double
+    
+    // Find area
+    var perimeter: Double{
+        return Double(2) * Double.pi * radius
+    }
+    
+    // Find surface area
+    var area: Double{
+        return Double.pi * pow(radius, 2)
+    }
+}
+
+//Create a cirlce
+var aCircle = Circle.init(radius: 3)
+
+// Find area and surface area
+aCircle.perimeter
+aCircle.area
+
+struct Triangle {
+    
+    //MARK: Properties
+    
+    var side1: Double
+    var side2: Double
+    var base: Double
+    var height: Double
+    
+    // Find perimeter
+    var perimeter: Double{
+        return side1 + side2 + base
+    }
+    
+    // Find area
+    var area: Double{
+        return 0.5 * height * base
+    }
+}
+
+var aTriangle = Triangle(side1: 3, side2: 4, base: 5, height: 6)
+
+aTriangle.area
+aTriangle.perimeter
 /*:
  [Previous: Area Example - Rectangle](@previous) | Page 4
  */
